87c1562a16cb5db97e46ab6e5b72e20508eecb54
Franco Romero Brewer <francoromerobrewer12@gmail.com>
2023-04-17 08:07:03 +0200

    Create configuration file and use it in team view

---
+++ src/configuration.js
@@ -0,0 +1,122 @@
+export const configuration = {
+    team: {
+        person_name: 'CAROLINA ACQUILA',
+        person_role: 'Founder & CEO',
+        person_image: '',
+        person_description: [
+            {
+                id: 0,
+                text: 'Mrs. Acquila has an extensive background in providing relocation, travel, real estate investment services, and business brokerage for prospective US investors from Latin America. Since arriving in the US in 2018, she has co-founded two travel agencies as well as two businesses specializing in helping Latin American investors and Hispanic businesswomen invest and succeed in the US.'
+            },
+            {
+                id: 1,
+                text: 'Prior to her US ventures, she worked as a founding partner of a law firm where she handled the civil, commercial, and immigration practices and was a co-founder of a family-owned transportation company, both in Buenos Aires, Argentina. She was a paralegal before that and also fulfilled the role of Operations Manager with another family-owned transportation business.'
+            },
+            {
+                id: 2,
+                text: 'Mrs. Acquila holds a law diploma from Argentina and has a Florida realtor license and business broker license.'
+            }
+        ],
+
+        experience_title: 'PROFESSIONAL EXPERIENCE',
+        experience_data: [
+            {
+                id: 3,
+                text: '2023 – Present: Link USA LLC, CEO, Sole Member & Founder,  (FL, USA)'
+            },
+            {
+                id: 4,
+                text: '2022 – Present: Women in Business Group LLC, Co-Founder, (FL, USA)'
+            },
+            {
+                id: 5,
+                text: '2022 – Present: One Veyt LLC, Co-Founder & President, (FL, USA)'
+            },
+            {
+                id: 6,
+                text: '2021 – Present: Casa Argentina en Florida Central LLC, Founder, (FL, USA)'
+            },
+            {
+                id: 7,
+                text: '2020 – Present: Carolina Acquila LLC, CEO & Founder, (FL, USA)'
+            },
+            {
+                id: 8,
+                text: '2018 – 2021: Magic Holidays, Co-Founder & Managing Partner, (FL, USA)'
+            },
+            {
+                id: 9,
+                text: '2010 – 2018: Estudio Juridico Acquila, Araya & Prieto, Founding Partner, (Buenos Aires, Argentina)'
+            },
+            {
+                id: 10,
+                text: '2005 – 2018: Via Ramos SRL, Co-Founder & Managing Partner, (Buenos Aires, Argentina)'
+            },
+            {
+                id: 11,
+                text: '2002 – 2004: Estudio Juridico Dra Elsa Carrera, Paralegal Employee, (Buenos Aires, Argentina)'
+            },
+            {
+                id: 12,
+                text: '1997 – 2002: Family Turismo, Operations Manager, (Buenos Aires, Argentina)'
+            },
+        ],
+
+        education_section_title: 'EDUCATION & MEMBERSHIPS',
+        education_content_sections: [
+            {
+                id: 13,
+                title: 'EDUCATION',
+                section_data: [
+                    {
+                        id: 14,
+                        text:'2022: Climer School / Gold Coast School Real Estate and Business Broker Training; Florida Realtor License; Florida Business Broker License'
+                    },
+                    {
+                        id: 15,
+                        text:'2021: Certifications in B1/B2 Visa Extensions, Immigration Updates, Immigration Paralegal Training, Employment based Residency by the Paralegal Institutes of America'
+                    },
+                    {
+                        id: 16,
+                        text:'2020: Florida Realtor License; Florida Business Broker License'
+                    },
+                    {
+                        id: 17,
+                        text:'2016: Law Diploma, Universidad de Moron, Buenos Aires, Argentina'
+                    },
+                ]
+            },
+            {
+                id: 18,
+                title: 'MEMBERSHIPS',
+                section_data: [
+                    {
+                        id: 19,
+                        text: '2022 - Today: Women’s Council of Realtors, Orlando, FL'
+                    },
+                    {
+                        id: 20,
+                        text: '2021 - Today: Volunteer of “Together and Free”, a non-profit association for low income immigrant families'
+                    },
+                    {
+                        id: 21,
+                        text: '2019 - Today: Volunteer at the Argentinean Elementary school in Miami'
+                    },
+                    {
+                        id: 22,
+                        text: '2018 - Today: Volunteer for the Orlando Chapter of the Argentinian Consulate of Miami'
+                    },
+                    {
+                        id: 23,
+                        text: '2016 - Today: Argentinian Bar Association'
+                    },
+                ]
+            }
+        ],
+
+        company_banner_title: 'LINK UP WITH US',
+        company_banner_image: '',
+        company_banner_text: 'To find out more about how we can help you with your move to the USA, or to learn more about our franchise opportunities, please get in touch. We would love to connect.',
+        company_banner_button_text: 'CONTACT US'
+    }
+}
